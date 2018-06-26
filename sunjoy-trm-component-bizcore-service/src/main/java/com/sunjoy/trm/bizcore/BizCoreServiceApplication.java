c7aa497196fa7aab6a74c09c386d5392ca114739
habibliu <740033486@qq.com>
2018-06-26 09:10:25 +0800

    文档提交

--- sunjoy-trm-component-bizcore-service/src/main/java/com/sunjoy/trm/bizcore/BizCoreServiceApplication.java
+++ sunjoy-trm-component-bizcore-service/src/main/java/com/sunjoy/trm/bizcore/BizCoreServiceApplication.java
@@ -1,4 +1,4 @@
-package com.sunjoy.trm.master;
+package com.sunjoy.trm.bizcore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
